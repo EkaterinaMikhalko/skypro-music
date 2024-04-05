4a96ba1f04609ed17516397079611c63cbcad6f6
EkaterinaMikhalko <mikhalkoea@gmail.com>
2024-04-05 03:37:01 +0300

    bar done

---
+++ src/components/Bar/Bar.tsx
@@ -0,0 +1,91 @@
+import Link from "next/link";
+import styles from "./Bar.module.css"
+import classNames from "classnames";
+
+export default function Bar () {
+    return (
+        <div className={styles.bar}>
+        <div className={styles.barContent}>
+          <div className={styles.barPlayerProgress} />
+          <div className={styles.barPlayerBlock}>
+            <div className={styles.barPlayer}>
+              <div className={styles.playerControls}>
+                <div className={styles.playerBtnPrev}>
+                  <svg className={styles.playerBtnPrevSvg}>
+                    <use xlinkHref="img/icon/sprite.svg#icon-prev" />
+                  </svg>
+                </div>
+                <div className={classNames (styles.playerBtnPlay, styles._btn)}>
+                  <svg className={styles.playerBtnPlaySvg}>
+                    <use xlinkHref="img/icon/sprite.svg#icon-play" />
+                  </svg>
+                </div>
+                <div className={styles.playerBtnNext}>
+                  <svg className={styles.playerBtnNextSvg}>
+                    <use xlinkHref="img/icon/sprite.svg#icon-next" />
+                  </svg>
+                </div>
+                <div className={classNames (styles.playerBtnRepeat, styles._btnIcon)}>
+                  <svg className={styles.playerBtnRepeatSvg}>
+                    <use xlinkHref="img/icon/sprite.svg#icon-repeat" />
+                  </svg>
+                </div>
+                <div className={classNames (styles.playerBtnShuffle, styles._btnIcon)}>
+                  <svg className={styles.playerBtnShuffleSvg}>
+                    <use xlinkHref="img/icon/sprite.svg#icon-shuffle" />
+                  </svg>
+                </div>
+              </div>
+              <div className={styles.playerTrackPlay}>
+                <div className={styles.trackPlayContain}>
+                  <div className={styles.trackPlayImage}>
+                    <svg className={styles.trackPlaySvg}>
+                      <use xlinkHref="img/icon/sprite.svg#icon-note" />
+                    </svg>
+                  </div>
+                  <div className={styles.trackPlayAuthor}>
+                    <Link className={styles.trackPlayAuthorLink} href="http://">
+                      Ты та...
+                    </Link>
+                  </div>
+                  <div className={styles.trackPlayAlbum}>
+                    <Link className={styles.trackPlayAlbumLink} href="http://">
+                      Баста
+                    </Link>
+                  </div>
+                </div>
+                <div className={styles.trackPlayLikeDis}>
+                  <div className={classNames (styles.trackPlayLike, styles._btnIcon)}>
+                    <svg className={styles.trackPlayLikeSvg}>
+                      <use xlinkHref="img/icon/sprite.svg#icon-like" />
+                    </svg>
+                  </div>
+                  <div className={classNames (styles.trackPlayDislike, styles._btnIcon)}>
+                    <svg className={styles.trackPlayDislikeSvg}>
+                      <use xlinkHref="img/icon/sprite.svg#icon-dislike" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.barVolumeBlock}>
+              <div className={styles.volumeContent}>
+                <div className={styles.volumeImage}>
+                  <svg className={styles.volumeSvg}>
+                    <use xlinkHref="img/icon/sprite.svg#icon-volume" />
+                  </svg>
+                </div>
+                <div className={classNames (styles.volumeProgress, styles._btn )}>
+                  <input
+                    className={classNames (styles.volumeProgressLine, styles._btn)}
+                    type="range"
+                    name="range"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
